e6d29f40d3ab348a31507446da98373c5fe3985e
Lorena Ponsico <lorena.poca@gmail.com>
2023-06-18 11:49:47 +0200

    n3 ex9 URL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { PanellComponent } from './home/panell/panell.component';
-import { BudgetService } from './home/services/budget.service';
 import{ ReactiveFormsModule} from '@angular/forms';
-import { WelcomeComponent } from './welcome/welcome.component'
-import { AppRoutingModule } from './app-routing.module';
-import { ModalComponent } from './modal/modal.component';
-import { PressupostListComponent } from './home/pressupost-list/pressupost-list.component';
 import { FormsModule } from '@angular/forms';
 
-
-
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { BudgetService } from './home/services/budget.service';
+import { PanellComponent } from './home/panell/panell.component';
+import { PressupostListComponent } from './home/pressupost-list/pressupost-list.component';
+import { WelcomeComponent } from './welcome/welcome.component'
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
